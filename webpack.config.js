cbc2c9eb29e16e5c1c087a46a4359367bde3297c
Barrellon Sylvain <sylvain.barrellon@hotmail.fr>
2023-07-25 17:23:37 +0200

    ajout des scripts et styles pour le back

--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,10 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('main_script', './assets/script/main.ts')
-    .addStyleEntry('main_style', './assets/style/main.scss')
+    .addEntry('main_script', './assets/front/script/main.ts')
+    .addStyleEntry('main_style', './assets/front/style/main.scss')
+    .addEntry('back_script', './assets/back/script/main.ts')
+    .addStyleEntry('back_style', './assets/back/style/main.scss')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
